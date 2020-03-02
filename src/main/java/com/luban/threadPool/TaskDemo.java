fa54356a2fd747d6da5aa5c5d2b977ef4e7b3a5c
westear <xyq667129@163.com>
2020-03-02 22:51:45 +0800

    鲁班学院的一些例子

---
+++ src/main/java/com/luban/threadPool/TaskDemo.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class TaskDemo implements Runnable{
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+" is running");
+        try {
+            TimeUnit.SECONDS.sleep(new Random().nextInt(10));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
